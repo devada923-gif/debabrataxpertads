633e4a7618df8faad81dc0025d042f3e57e3c596
devada923-gif <devada923@gmail.com>
2026-08-30 17:27:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/debabrataxpertads/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
